f88d701217c6006a7b9541782234fc7e8daa350c
marianna325655656 <marianna_r@purlin.com>
2023-10-26 17:39:54 +0400

    Calculate numbers from 1 to 100 bu using while, do while, for loops

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,71 +1,42 @@
-﻿
-//print planets distance by using if else, switch
+﻿// Calculate numbers from 1 to 100 bu using while, do whilw, for loops
 
-
-//string[] planets = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
-
-//Console.Write("Enter the planet name: ");
-//string input = Console.ReadLine();
-
-//switch (input)
-//{
-//    case "Mercury":
-//        Console.WriteLine("The distance is 57,900,000 km");
-//        break;
-//    case "Venus":
-//        Console.WriteLine("The distance is 108,200,000 km");
-//        break;
-//    case "Earth":
-//        Console.WriteLine("The distance is 149,600,000 km");
-//        break;
-//    case "Mars":
-//        Console.WriteLine("The distance is 227,900,000 km");
-//        break;
-//    case "Jupiter":
-//        Console.WriteLine("The distance is 778,600,000 km");
-//        break;
-//    case "Saturn":
-//        Console.WriteLine("The distance is 1,433,500,000 km");
-//        break;
-//    case "Uranus":
-//        Console.WriteLine("The distance is 2,872,500,000 km");
-//        break;
-//    case "Neptune":
-//        Console.WriteLine("The distance is 4,495,100,000 km");
-//        break;
-
-//    default:
-//        Console.WriteLine("The planet is not found");
-//        break;
-//}
-
-Dictionary<string, string> planetNames = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+int x = 1;
+int sum = 0;
+while (x <= 100)
 {
-    { "Mercury", "The distance is 57,900,000 km" },
-    { "Venus", "The distance is 108,200,000 km" },
-    { "Earth", "The distance is 149,600,000 km" },
-    { "Mars", "The distance is 227,900,000 km" },
-    { "Jupiter", "The distance is 778,600,000 km" },
-    { "Saturn", "The distance is 1,433,500,000 km" },
-    { "Uranus", "The distance is 2,872,500,000 km" },
-    { "Neptune", "The distance is 4,495,100,000 km" }
-};
-
-Console.Write("Enter the planet name: ");
-string input1 = Console.ReadLine()?.Trim();
-
-if (!string.IsNullOrEmpty(input1))
-{
-    if (planetNames.TryGetValue(input1, out string planetDistance))
-    {
-        Console.WriteLine(planetDistance);
-    }
-    else
-    {
-        Console.WriteLine("The planet name is not found.");
-    }
+    sum = sum + x;
+    x++;
 }
-else
+Console.WriteLine("sum: " + sum);
+
+int x1 = 1;
+int sum1 = 0;
+do
 {
-    Console.WriteLine("Invalid input");
+    sum1 = sum1 + x1;
+    x1++;
+
 }
+while (x <= 100);
+Console.WriteLine("sum: " + sum1);
+
+int sum2 = 0;
+for (int i = 1; i <= 100; i++)
+{
+    sum2 = sum2 + i;
+
+}
+Console.WriteLine("sum: " + sum2);
+
+int SumOfNumbers(int minNumber, int maxNumber)
+{
+    int sum3 = 0;
+    for (int i = minNumber; i <= maxNumber; i++)
+    {
+        sum3 = sum3 + i;
+    }
+    return sum3;
+}
+
+
+Console.WriteLine("sum3: " + SumOfNumbers(1, 100));
